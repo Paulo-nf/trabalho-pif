8b9ce46e82811a27b1e6bf39440a62a900810f3e
Paulo Nery <paulo.nery97@gmail.com>
2025-11-12 20:37:57 -0300

    implementacao inicial de colisao com parede

--- src/main.c
+++ src/main.c
@@ -15,6 +15,23 @@ int pacmanX = 34, pacmanY = 12;
 int incX = 1, incY = 1;
 #define PACMAN 'P'
 
+#define SIZEX 80
+#define SIZEY 24
+
+int walls[SIZEX][SIZEY] = {0};
+
+void printWalls(int walls[SIZEX][SIZEY]){
+    for(int i = 0; i < SIZEX; i++){
+        for(int j = 0; j < SIZEY; j++){
+            if(walls[i][j] == 1){
+                screenSetColor(CYAN, BLUE);
+                screenGotoxy(i, j);
+                printf(" ");
+            }
+        }
+    }
+}
+
 void printChar(char c, int nextX, int nextY) {
     screenSetColor(CYAN, DARKGRAY);
     screenGotoxy(pacmanX, pacmanY);
@@ -33,18 +50,33 @@ enum direction {
 void moveChar(int *newX, int *newY, int d, char c) {
     screenSetColor(CYAN, DARKGRAY);
     screenGotoxy(*newX, *newY);
-    printf(" ");
     switch (d) {
         case DIR_W:
+            if(walls[*newX][*newY-1] == 1){
+                return;
+            };
+            printf(" ");
             *newY = (*newY - incY < MINY + 1) ? MINY + 1 : *newY - incY;
             break;
         case DIR_A:
+            if(walls[*newX-1][*newY] == 1){
+                return;
+            };
+            printf(" ");
             *newX = (*newX - incX < MINX + 1) ? MINX + 1 : *newX - incX;
             break;
         case DIR_S:
+            if(walls[*newX][*newY+1] == 1){
+                return;
+            };
+            printf(" ");
             *newY = (*newY + incY > MAXY - 1) ? MAXY - 1 : *newY + incY;
             break;
         case DIR_D:
+            if(walls[*newX+1][*newY] == 1){
+                return;
+            };
+            printf(" ");
             *newX = (*newX + incX > MAXX - 2) ? MAXX - 2 : *newX + incX;
             break;
     }
@@ -65,6 +97,9 @@ int main() {
     timerInit(50);
 
     printChar(PACMAN, pacmanX, pacmanY);
+    walls[30][5] = 1;
+    printWalls(walls);
+
     screenUpdate();
 
     while (ch != 10) // enter
